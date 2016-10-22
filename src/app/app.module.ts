7e9b8530cefb3f56d32d8fe401dcb5888b1fc023
Benjamin Scabbia <benscabbia@gmail.com>
2016-10-22 16:12:23 +0100

    Added navigation bar Added AppSettings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
+    NavigationComponent,
     AppComponent
   ],
   imports: [

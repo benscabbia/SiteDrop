3ea575fb6958ca7ce71adfbd013f0078b918329d
Benjamin Scabbia <benscabbia@gmail.com>
2016-10-25 20:30:02 +0100

    Added auth guard Added protected root

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,3 +1,5 @@
+import { AuthGuard } from './authentication/auth.guard';
+import { ProfileComponent } from './authentication/profile/profile.component';
 import { SignupComponent } from './authentication/signup.component';
 import { LoginComponent } from './authentication/login.component';
 import { HomeComponent } from './home.component';
@@ -7,7 +9,8 @@ import { Routes, RouterModule } from '@angular/router';
 const APP_ROUTES: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'signup', component: SignupComponent }
+    { path: 'signup', component: SignupComponent },
+    { path: 'profile', component: ProfileComponent, canActivate:[AuthGuard] },
 
 ];
 

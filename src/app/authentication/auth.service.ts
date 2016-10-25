36a4fead516dbd8e718d3dc1aaf28123b318e760
Benjamin Scabbia <benscabbia@gmail.com>
2016-10-25 20:30:15 +0100

    Implemented auth service

--- src/app/authentication/auth.service.ts
+++ src/app/authentication/auth.service.ts
@@ -3,25 +3,60 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject, Observable } from 'rxjs/Rx';
 
+declare var firebase: any;
 
-@Injectable() 
+@Injectable()
 export class AuthService {
     constructor(private router: Router) { }
 
-    signupUser(user: User){
-        // todo
+    signupUser(user: User) {
+        firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+            .catch(function (error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // todo
+                console.log(errorCode);
+                console.log(errorMessage);
+            });
     }
 
-    signinUser(user: User){
-        // todo
+    signinUser(user: User) {
+        firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+            .catch(function (error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // todo
+                console.log(errorCode);
+                console.log(errorMessage);
+            });
     }
 
-    logout(){
+    logout() {
         // todo
+        firebase.auth().signOut()
+            .then(function () {
+                // Sign-out successful.
+                // todo
+            }, function (error) {
+                // An error happened.
+                // todo
+            });
     }
 
-    isAuthenticated(): Observable<boolean>{
-        // todo
-        return;
+    isAuthenticated(): Observable<boolean> {
+
+        const subject = new Subject<boolean>();
+
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                subject.next(true); 
+            } else {
+                subject.next(false);                 
+            }
+        });
+
+        return subject.asObservable();
     }
 }

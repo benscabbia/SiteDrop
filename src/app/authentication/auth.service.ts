ea9286686d3120336001a26790dff5863f4713b5
Benjamin Scabbia <benscabbia@gmail.com>
2016-11-01 21:14:37 +0000

    User profile created on signup (WIP)

--- src/app/authentication/auth.service.ts
+++ src/app/authentication/auth.service.ts
@@ -1,32 +1,34 @@
+import { Validators } from '@angular/forms';
 import { User } from './user.interface';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject, Observable } from 'rxjs/Rx';
+import { Headers, Http, Response } from '@angular/http';
 
 declare var firebase: any;
 
 @Injectable()
 export class AuthService {
-    constructor(private router: Router) { }
+    constructor(private router: Router, private http: Http) { }
 
-    public signupUser(user: User) {
+    public signupUser(user: User): void {
         firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
             .then(
-                success => {
-                // set uid
-                user['$key'] = success.uid;
-                // create profile
-                let profileData = Object.assign({}, user);
-                let userKey = profileData['$key'];
+            success => {
 
+                //set uid
+                user['key'] = success.uid;
+                // let profileData = Object.assign({}, user);
+                // let userKey = profileData['$key'];
+
+                this.storeProfile(user).subscribe(
+                    profile => console.log("Profile Created: " + profile),
+                    error => console.log(error)
+                );
                 // post  profileData under: '/profiles/uid/profileData
 
-                console.log(profileData);
-                console.log(userKey);
-
-
                 this.router.navigate(['/profile']);
-                }
+            }
             )
             .catch(function (error) {
                 // Handle Errors here.
@@ -38,12 +40,28 @@ export class AuthService {
             });
     }
 
+    private storeProfile(userProfile: User): Observable<Response> {
+        // create profile
+        let body = JSON.stringify(userProfile);
+
+        let headers = new Headers({'Content-Type':'application/JSON'});
+        //let profileData = Object.assign({}, userProfile);
+        let userKey = userProfile['key'];
+        //let body = {"hello":"world"};
+        console.log(body);
+        //firebase.database().ref('profiles/' + userKey).set(body);
+        return this.http.post('https://sitedrop-c39b3.firebaseio.com/profiles/' + userKey + '.json', body, headers)
+        .map((data: Response) => data.json());        
+        // console.log(profileData);
+        // console.log(userKey);
+    }
+
     public signinUser(user: User) {
         firebase.auth().signInWithEmailAndPassword(user.email, user.password)
-            .then( 
-                success => {
-                    this.router.navigate(['/dashboard'])
-                }
+            .then(
+            success => {
+                this.router.navigate(['/dashboard'])
+            }
             )
             .catch(function (error) {
                 // Handle Errors here.

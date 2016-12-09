59893b831af109c4c7daf3acb4f33f1db7e52633
Benjamin Scabbia <benscabbia@gmail.com>
2016-12-09 06:11:44 +0000

    addSite now correctly stores user websites

--- src/app/dashboard/site.service.ts
+++ src/app/dashboard/site.service.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs/Rx';
 import { Site } from './site.interface';
 import { AngularFire } from 'angularfire2';
 import { Http } from '@angular/http';
@@ -9,26 +10,25 @@ export class SiteService {
 
     constructor(private http: Http, private af: AngularFire) {}
 
-    public addSite(site: Site): boolean {
+    public addSite(site: Site): void {
+
+        
         site.userKey = 'S9mLmYSXTJM5Ppek7TkIvmXQUAA2';
         // site.siteKey;
 
         site.siteName = 'My site';
         site.siteDescription = 'this is description text';
         site.siteUrl = 'https://example.com';
-
+        console.log(site);        
         let body = Object.assign({}, site);
-        console.log(body);
-
-        this.af.database.object('sites/' + site.userKey + '/').set(body)
+        console.log(body);        
+        this.af.database.list('sites/' + site.userKey + '/').push(body)
             .then(success => {
-                console.log('Site created successfully');
-                return true;
+                console.log('Site created successfully');                
             })
             .catch(error => {
                 console.log('Error in adding your site');
-                return false;
-            });
-        return false;
+                console.log(error);               
+            });            
     }
 }
